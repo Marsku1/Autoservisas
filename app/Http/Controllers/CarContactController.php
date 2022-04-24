28531bf0a81c01bb6db8be2c2ca799279df85630
Marsku1 <79592172+Marsku1@users.noreply.github.com>
2022-04-24 15:54:43 +0300

    .

--- app/Http/Controllers/CarContactController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CarProfilesController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::resource("/carProfile", CarProfilesController::class);

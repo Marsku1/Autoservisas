afbde9d3a45eea649d282416fdcbdc42c0950012
Donadai <donatas.dailide1@gmail.com>
2022-05-29 20:31:09 +0300

    'update'

--- resources/views/CarProfileViews/index.blade.php
+++ resources/views/CarProfileViews/index.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 @section('content')
+@if(\Session::has('success'))
+<div class="alert alert-success">
+    <p>{{ \Session::get('success') }}</p>
+</div>
+@endif
     <div class="container">
         <div class="row">
 
